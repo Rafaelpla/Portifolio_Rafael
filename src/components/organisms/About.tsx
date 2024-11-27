e274c0a7db9d2275be6be6543ba70337ee39274f
Rafaelpla <rafasutildacosta@gmail.com>
2024-11-27 09:56:08 -0300

    Fix Responsivity

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -16,12 +16,12 @@ const About: React.FC<AboutProps> = ({ language }) => {
 
     return (
         <div className="flex md:items-center lg:items-start lg:pt-24 lg:space-x-12 lg:justify-center lg:flex-row pt-12 md:pt-0 flex-col md:gap-10 lg:gap-24 bg-stone-900 md:h-[740px] lg:h-screen">
-            <div className="md:pl-12 lg:pt-32 md:pt-24 p-8 md:p-0 text-white max-w-xl md:max-w-2xl text-justify lg:w-2/4 w-full space-y-8">
+            <div className="md:pl-12 lg:pt-32 md:pt-24 mx-auto md:mx-0 p-8 md:p-0 text-white max-w-xl md:max-w-2xl text-justify lg:w-2/4 w-full space-y-8">
                 <h1 className="md:text-6xl text-4xl font-bold">{Welcome}</h1> 
                 <h1 className="md:text-2xl text-lg">{AboutText}</h1>
             </div>
             <div className="flex lg:pr-0 lg:pt-32 lg:pl-0 p-12 md:p-0 content-center text-white space-y-8 justify-center">
-                <img className="md:w-72 sm:w-64 w-52 rounded-xl mx-auto" width={200} src="src/assets/foto.jpeg" alt="Rafael Sutil" />
+                <img className="md:w-72 sm:w-64 w-52 rounded-xl md:mx-auto" width={200} src="src/assets/foto.jpeg" alt="Rafael Sutil" />
             </div>
         </div>
     )
